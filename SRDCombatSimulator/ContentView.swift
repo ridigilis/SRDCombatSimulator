ed6e592069cfeb0c7c230385f727563fae4c876f
ridigilis <ridigilis@ridigilis.dev>
2024-04-13 16:48:03 -0500

    Initial Commit

---
+++ SRDCombatSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SRDCombatSimulator
+//
+//  Created by Ricky David Groner II on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SRDCombatSimulator/SRDCombatSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRDCombatSimulatorApp.swift
+//  SRDCombatSimulator
+//
+//  Created by Ricky David Groner II on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SRDCombatSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
